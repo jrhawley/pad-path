85fcce539aef253e1fb8398ee06ba342e77a367b
James Hawley <developer@jrhawley.ca>
2021-11-19 13:32:25 -0500

    Refactoring `revert_path()` into new module

--- src/path/mod.rs
+++ src/path/mod.rs
@@ -1,6 +1,5 @@
 //! Read, write, and process the current `$PATH`.
 
-use std::io;
 #[cfg(target_os = "windows")]
 use std::os::windows::ffi::OsStrExt;
 
@@ -12,15 +11,3 @@ pub mod read;
 pub mod remove;
 pub mod revert;
 pub mod write;
-
-use self::{history::get_nth_last_revision, write::replace_path};
-
-/// Revert to an earlier PATH
-/// This makes use of the `.path_history` file
-pub fn revert_path(revision: u128, dry_run: bool, add_to_history: bool) -> io::Result<()> {
-    // look up an old `$PATH` from the path history
-    let newpath = get_nth_last_revision(revision)?;
-
-    // replace the current path with the revised one
-    replace_path(newpath, dry_run, add_to_history)
-}
--- src/path/revert.rs
+++ src/path/revert.rs
@@ -1,1 +1,16 @@
 //! Revert `$PATH` to a previous value.
+
+use std::io;
+
+use super::{history::get_nth_last_revision, write::replace_path};
+
+/// Revert to an earlier `$PATH`
+///
+/// This makes use of the `.path_history` file
+pub fn revert_path(revision: u128, dry_run: bool, add_to_history: bool) -> io::Result<()> {
+    // look up an old `$PATH` from the path history
+    let newpath = get_nth_last_revision(revision)?;
+
+    // replace the current path with the revised one
+    replace_path(newpath, dry_run, add_to_history)
+}
